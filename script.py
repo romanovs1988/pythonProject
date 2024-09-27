9b4ceb3fa291c14704984025e8eb14a741949f26
Sergey Romanov <dimetrius465@gmail.com>
2024-09-27 15:54:00 +0300

    2-ой модуль

--- script.py
+++ script.py
@@ -3,11 +3,13 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def run_script():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    service = Service(executable_path=ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
 
     driver.get("http://skillbox.ru")
     driver.quit()
+
 
 if __name__ == '__main__':
     run_script()

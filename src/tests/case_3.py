fdebcd18449b41dd58f6399c124ba177c12554ff
Sergey Romanov <dimetrius465@gmail.com>
2024-10-30 17:41:08 +0300

    4-й модуль

--- src/tests/case_3.py
+++ src/tests/case_3.py
@@ -1,8 +1,11 @@
 import time
+from telnetlib import EC
 
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from conftest import get_driver
 
 class TestClick:
@@ -12,14 +15,14 @@ class TestClick:
         time.sleep(5)
         driver.find_element(By.CSS_SELECTOR, '[value="Python"]').click()
         time.sleep(5)
-        driver.find_element(By.CSS_SELECTOR, '#search_stars').send_keys('>20000')
-        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, '#search_stars').send_keys('>1000')
+
         driver.find_element(By.CSS_SELECTOR, '#search_filename').send_keys('environment.yml')
-        time.sleep(5)
 
-        driver.find_element(By.XPATH, '//div[contains(@class, "d-flex d-md-block flex-shrink-0 pt-2 pt-md-0")]/child::button').click()
+        driver.find_element(By.XPATH, '//div[contains(@class, "form-group flattened")]/descendant::button').click()
+
         #time.sleep(5)
-
+        #driver.find_element(By.XPATH, '//*[contains(@class, "form-control input-block search-page-input js-advanced-search-input js-advanced-search-prefix")]').send_keys(Keys.ENTER)
         #action_chains = webdriver.ActionChains(driver)
         #time.sleep(13)
         #action_chains.move_to_element(driver.find_element(By.XPATH, '//div[contains(@class, "d-flex d-md-block flex-shrink-0 pt-2 pt-md-0")]/child::button')).perform()
--- src/tests/case_4.py
+++ src/tests/case_4.py
@@ -8,16 +8,16 @@ class TestSlider:
     def test_slider(self, get_driver):
         driver = get_driver
         driver.get('https://skillbox.ru/code/')
-        time.sleep(30)
-        driver.find_element(By.XPATH, '//label[contains(@value, "profession")]/child::input').click()
-        el = driver.find_element(By.XPATH, '//div[contains(@aria-valuetext, "6")]/child::button')
+        time.sleep(5)
+        driver.find_element(By.XPATH, '(//input[contains(@value, "profession")])').click()
+        el = driver.find_element(By.XPATH, '(//div[contains(@aria-valuetext, "1")]/child::button)')
         action_chains = webdriver.ActionChains(driver)
         action_chains \
             .click_and_hold(el) \
             .move_by_offset(xoffset=5, yoffset=0) \
             .perform()
         action_chains.release().perform()
-        el = driver.find_element(By.XPATH, '//div[contains(@aria-valuetext, "24")]/child::button')
+        el = driver.find_element(By.XPATH, '(//div[contains(@aria-valuetext, "24")]/child::button)')
         action_chains = webdriver.ActionChains(driver)
         action_chains \
             .click_and_hold(el) \

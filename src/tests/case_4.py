b8a156860e444f03e1acb8277811ae1310edb466
Sergey Romanov <dimetrius465@gmail.com>
2024-11-04 11:44:18 +0300

    4-й модуль

--- src/tests/case_4.py
+++ src/tests/case_4.py
@@ -8,25 +8,16 @@ class TestSlider:
     def test_slider(self, get_driver):
         driver = get_driver
         driver.get('https://skillbox.ru/code/')
-        time.sleep(5)
-        driver.find_element(By.CSS_SELECTOR, '#employment').click()
-        driver.find_element(By.CSS_SELECTOR, '[aria-label="Вернуться наверх страницы"]').click()
-        #driver.find_element(By.XPATH, '(//label[contains(@value, "profession")])').click()
-        """el = driver.find_element(By.XPATH, '//div[contains(@aria-valuetext, "1")]')
+
+        radio = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-radio-field__value ui-radio-field__value--small")])[2]').click()
+        slider_1 = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-range__dot")])[1]')
         action_chains = webdriver.ActionChains(driver)
-        action_chains \
-            .click_and_hold(el) \
-            .move_by_offset(xoffset=5, yoffset=0) \
-            .perform()
-        action_chains.release().perform()"""
-        """el = driver.find_element(By.XPATH, '//div[contains(@aria-valuetext, "24")]')
+        action_chains.click_and_hold(slider_1).move_by_offset(100, 0).perform()
+        slider_2 = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-range__dot")])[2]')
         action_chains = webdriver.ActionChains(driver)
-        action_chains \
-            .click_and_hold(el) \
-            .move_by_offset(xoffset=-12, yoffset=0) \
-            .perform()
-        action_chains.release().perform()"""
-        #driver.find_element(By.XPATH, '(//span[contains(text(), "Android")]/ancestor::label)').click()
+        action_chains.click_and_hold(slider_2).move_by_offset(100, 0).perform()
+        fullbox = driver.find_element(By.XPATH, '//*[@id="#app"]/main/div[1]/div[2]/div/div[1]/div[1]/div[6]/button').click()
+        checkbox = driver.find_element(By.XPATH, '(//*[contains(@class, "filter-checkboxes-list__value")])[28]').click()
         pass
 
 

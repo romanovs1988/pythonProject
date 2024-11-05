24cd3f90c00d7c8769a0a43c767ae46f12f5176c
Sergey Romanov <dimetrius465@gmail.com>
2024-11-05 18:22:29 +0300

    4-й модуль

---
+++ src/tests/module_5.py
@@ -0,0 +1,93 @@
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import element_attribute_to_include
+from selenium.common import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver import Keys
+from conftest import get_driver
+
+class TestValidate:
+    def test_1(self, get_driver):
+        driver = get_driver
+        expected_value = ' bug' or ' Bug'
+
+        driver.get('https://github.com/microsoft/vscode/issues')
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, '#repository-input').send_keys('in:title')
+        time.sleep(5)
+        field = driver.find_element(By.CSS_SELECTOR, '#repository-input')
+        field.send_keys(expected_value + Keys.ENTER)
+        time.sleep(5)
+        assert field.get_attribute('value') == ' bug' or ' Bug'
+        pass
+
+    def test_2(self, get_driver):
+        driver = get_driver
+        expected_value = 'bpasero'
+        driver.get('https://github.com/microsoft/vscode/issues')
+        driver.find_element(By.CSS_SELECTOR, '[aria-label="Filter by author"]').click()
+        field = driver.find_element(By.XPATH, '//*[contains(@class, "UnstyledTextInput-sc-14ypya-0 kbCLEG")]')
+        field.send_keys(expected_value + Keys.ENTER)
+        assert field.get_attribute('value') == 'bpasero'
+
+        #time.sleep(10)
+        #driver.find_element(By.XPATH, '//*[contains(@class, "UnstyledTextInput-sc-14ypya-0 kbCLEG")]').send_keys(Keys.ENTER)
+
+        pass
+
+    def test_3(self, get_driver):
+        driver = get_driver
+        driver.get('https://github.com/search/advanced')
+        expected_value = '>20000'
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, '[value="Python"]').click()
+        time.sleep(5)
+        field = driver.find_element(By.CSS_SELECTOR, '#search_stars')
+        field.send_keys(expected_value)
+        assert field.get_attribute('value') == '>20000'
+
+        driver.find_element(By.CSS_SELECTOR, '#search_filename').send_keys('environment.yml')
+
+        #driver.find_element(By.XPATH, '//div[contains(@class, "form-group flattened")]/descendant::button').click()
+        pass
+
+
+    def test_4(self, get_driver):
+        driver = get_driver
+        driver.get('https://skillbox.ru/code/')
+
+        radio = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-radio-field__value ui-radio-field__value--small")])[2]').click()
+        slider_1 = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-range__dot")])[1]')
+        action_chains = webdriver.ActionChains(driver)
+        action_chains.click_and_hold(slider_1).move_by_offset(100, 0).perform()
+        slider_2 = driver.find_element(By.XPATH, '(//*[contains(@class, "ui-range__dot")])[2]')
+        action_chains = webdriver.ActionChains(driver)
+        action_chains.click_and_hold(slider_2).move_by_offset(100, 0).perform()
+        fullbox = driver.find_element(By.XPATH, '//*[@id="#app"]/main/div[1]/div[2]/div/div[1]/div[1]/div[6]/button').click()
+        checkbox = driver.find_element(By.XPATH, '(//*[contains(@class, "filter-checkboxes-list__value")])[28]')
+        checkbox.click()
+
+        assert checkbox.is_selected() is True
+        pass
+
+
+
+
+    def test_5(self, get_driver):
+        driver = get_driver
+        driver.get('https://github.com/microsoft/vscode/graphs/commit-activity')
+        action_chains = webdriver.ActionChains(driver)
+        time.sleep(13)
+        bar = action_chains.move_to_element(driver.find_element(By.CSS_SELECTOR, '[y="0"]'))
+        bar.perform()
+
+        assert bar.is_selected() is True
+        pass
+
+
+
+
+
+
+

270a745b92d9929c095b7da681d31b94cc96f498
Sergey Romanov <dimetrius465@gmail.com>
2024-12-19 20:55:01 +0300

    7-й модуль

--- Tests/homework/module_5.py
+++ Tests/homework/module_5.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 import allure
@@ -20,95 +21,101 @@ class TestValidate:
 
         with allure.step('Открытие страницы https://github.com/microsoft/vscode/issues'):
             page.goto('https://github.com/microsoft/vscode/issues')
-            #time.sleep(5)
         with allure.step('Заполнение формы данными'):
-            page.fill('#repository-input', "in:title bug or Bug") #('#repository-input', "in:title") ('#repository-input').send_keys('in:title')
-            #time.sleep(5)
+            page.fill('#repository-input', "in:title bug or Bug")
         with allure.step('Клик по кнопке ENTER'):
             page.press('#repository-input', "Enter")
+        with allure.step('Получите все названия задач.'):
+            issue_titles = page.locator("a[data-hovercard-type='issue']").all_inner_texts()
+        with allure.step('Проверьте, что каждая из задач содержит в названии слово "bug".'):
+            for title in issue_titles:
+                assert "bug" in title.lower(), f"'{title}' не содержит слово 'bug'"
         pass
 
     def test_2(self, page):
-
-        expected_value = 'bpasero'
         with allure.step('Переход на страницу https://github.com/microsoft/vscode/issues'):
             page.goto('https://github.com/microsoft/vscode/issues')
         with allure.step('Клик по найденному элементу'):
             page.click('[aria-label="Filter by author"]')
-            # wait for 1 second
-            #page.wait_for_timeout(3000)
         with allure.step('Заполнение формы данными'):
             page.fill('//*[contains(@class, "UnstyledTextInput__ToggledUnstyledTextInput-sc-14ypya-0 jkNcAv")]', "bpasero")
             page.wait_for_timeout(3000)
         with allure.step('Клик по кнопке ENTER'):
             page.press('//*[contains(@class, "UnstyledTextInput__ToggledUnstyledTextInput-sc-14ypya-0 jkNcAv")]', "Enter")
-       #expect(page.locator('//input[@id="repository-input"]')).to_contain_text('bpasero')
-       # assert expected_value.evaluate("node => node.innerText") == "bpasero"
-       # locator = page.locator('//input[@id="repository-input"]')
-       # expect(locator).to_have_attribute("type", "bpasero")
+        with allure.step('Получите все задачи'):
+            issue_authors = page.locator("a[data-hovercard-type='user']").all_inner_texts()
+        with allure.step('Проверьте, что автор всех задач введён в поиск ("bpasero").'):
+            for author in issue_authors:
+                assert author.lower() == "bpasero", f"Автор '{author}' не совпадает с 'bpasero'"
         pass
 
     def test_3(self, page):
 
-        with allure.step('Переход на страницу https://github.com/search/advanced'):
+        with allure.step('Переход на страницу Advanced search на GitHub.'):
             page.goto('https://github.com/search/advanced')
-        expected_value = '>20000'
-        #time.sleep(5)
-        with allure.step('Клик по найденному элементу'):
-            #page.click('[value="Python"]')
-            page.click('//select[@id="search_language"]')
-            page.wait_for_timeout(3000)
-            for i in range(19):
-                page.keyboard.press("ArrowDown")
-            page.wait_for_timeout(3000)
-            page.keyboard.press("Enter")
-            #page.click('//*[@id="search_language"]/optgroup[1]/option[19]')
+        with allure.step('В поле языка, на котором написан код, выберите Python.'):
+            language_selector = page.locator('//select[@id="search_language"]')
+            language_selector.select_option("Python")
 
-        with allure.step('Заполнение формы данными'):
+        with allure.step('В поле количества звёзд у репозитория выберите >20000.'):
             page.fill('#search_stars', ">20000")
-            # field.send_keys(expected_value)
-        #assert field.get_attribute('value') == '>20000'
-        with allure.step('Заполнение формы данными'):
+
+        with allure.step('В поле с названием файла выберите environment.yml.'):
             page.fill('#search_filename', "environment.yml")
-        # driver.find_element(By.XPATH, '//div[contains(@class, "form-group flattened")]/descendant::button').click()
+
         with allure.step('Нажать "Поиск"'):
             page.click('//div[contains(@class, "form-group flattened")]/descendant::button')
+        with allure.step('Соберите информацию по всем репозиториям.'):
+            repo_list = page.locator("ul.repo-list li").all_inner_texts()
+        with allure.step('Проверьте, что в списке отображаются репозитории с количеством звёзд >20000.'):
+            for repo in repo_list:
+                if " " in repo:
+                    parts = repo.split(" ")
+                    stars = parts[0]
+                    stars_count = int(stars[:-1]) * (1000 if stars.endswith('k') else 1)
+                    assert stars_count > 20000, f"Репозиторий '{repo}' имеет звёзд: {stars_count}"
         pass
 
     def test_4(self, page, go_to_url):
 
-        with allure.step('Переход на страницу https://skillbox.ru/code/'):
+        with allure.step('Перейдите на сайт Skillbox'):
             go_to_url('https://skillbox.ru/code/')
-        with allure.step('Клик по найденному элементу'):
+        with allure.step('Выбор радиокнопки "Профессия" в разделе "Тип обучения на платформе"'):
             page.click('(//*[contains\
             (@class, "ui-radio-field__value ui-radio-field__value--small")])[2]')
             page.wait_for_timeout(3000)
         with allure.step('Переместить slider_1'):
-            page.drag_and_drop('//*[contains(@aria-valuetext, "1")]', '//*[contains(@aria-valuetext, "6")]')
-            page.wait_for_timeout(3000)
+            duration_slider_1 = page.locator('//*[contains(@aria-valuetext, "1")]')
+            duration_slider_1.evaluate("el => el.setAttribute('style', 'right: 30%;')")
         with allure.step('Переместить slider_2'):
-            page.drag_and_drop('//*[contains(@aria-valuetext, "24")]', '//*[contains(@aria-valuetext, "12")]')
+            duration_slider_2 = page.locator('//*[contains(@aria-valuetext, "24")]')
+            duration_slider_2.evaluate("el => el.setAttribute('style', 'left: 30%;')")
         with allure.step('Клик по найденному элементу'):
             page.click('//*[@id="#app"]/main/div[1]/div[2]/div/div[1]/div[1]/div[6]/button')
         with allure.step('Клик по чекбоксу'):
-            checkbox = page.click('(//*[contains(@class, "filter-checkboxes-list__value")])[28]')
-        #assert checkbox is True
+            page.click('(//*[contains(@class, "filter-checkboxes-list__value")])[28]')
+        with allure.step('Проверка, что курсы соответствуют ожиданиям'):
+            expected_courses = ["DevOps-инженер", "DevOps-инженер"]
+            course_titles = page.locator(".courses-section__grid").all_inner_texts()
+            for expected_course in expected_courses:
+                assert expected_course in course_titles, f"DevOps-инженер '{expected_course}' не найден в списке."
         pass
 
     def test_5(self, page):
         with allure.step('Переход на страницу https://github.com/microsoft/vscode/graphs/commit-activity'):
             page.goto('https://github.com/microsoft/vscode/graphs/commit-activity')
 
-        with allure.step('Переместить курсор на выбранный элемент'):
-            element = page.locator('//div[@class="svg-tip n"]')
-            page.wait_for_timeout(1000)
-            #element.wait_for('//div[@class="svg-tip n"]', "380 commits the week of Mar 17")
-            #action_chains.move_to_element(get_driver.find_element(By.ID, 'commit-activity-master')).perform()
-            #action_chains.move_by_offset(-100, 0).perform()
-        with allure.step('Переместить курсор на выбранный элемент'):
-            page.mouse.move(-100, 0)
-            #tooltip = get_driver.find_element(By.XPATH, '//div[@class="svg-tip n"]')
-        #tooltip_text = '380 commits the week of Mar 17'
-        #assert tooltip_text in tooltip.text == '380 commits the week of Mar 17'
-        #wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[transform="translate(280, 0)"]')))
+        with allure.step('Найдите график активности коммитов'):
+            graph = page.locator('(//*[@class="viz"])[1]')
+        with allure.step('Наведите мышку на график (на определенную область)'):
+            first_bar = graph.locator('rect').nth(0)
+            first_bar.hover()
+            time.sleep(1)
+        with allure.step('Получите текст тултипа'):
+            tooltip_locator = page.locator('//div[@class="svg-tip n"]')
+            tooltip_text = tooltip_locator.inner_text()
+        with allure.step('Проверка содержимого тултипа'):
+            expected_values = "8 commits the week of Dec 24"
+
+            assert expected_values in tooltip_text, f"Тултип не содержит ожидаемое значение: {expected_values}. Найдено: {tooltip_text}"
         pass

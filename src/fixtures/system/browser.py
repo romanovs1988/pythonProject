a602fb2e8fabe0e142de8b9813c4624468cbfc4d
Sergey Romanov <dimetrius465@gmail.com>
2025-01-28 19:30:55 +0300

    changes

--- src/fixtures/system/browser.py
+++ src/fixtures/system/browser.py
@@ -50,15 +50,22 @@ def go_to_url(page):
         page.goto(url, wait_until='domcontentloaded')
     return callback
 
-# @pytest.fixture()
-# def proxy():
-#     server = Server('C:\Users\Serjan777\miniconda3\Lib\site-packages\browsermobproxy')
-#     server.start()
-#     proxy = server.create_proxy()
-#     options = webdriver.ChromeOptions()
-#     options.add_argument(f'--proxy-server={proxy.proxy}')
-#     driver = webdriver.Chrome(options=options)
-#
-#     yield driver
-#     driver.quit()
-#     server.stop()
+@pytest.fixture()
+def send_500_response():
+    server = Server(r"C:\Program Files (x86)\browsermob-proxy-2.1.4-bin")
+    server.start()
+    proxy = server.create_proxy()
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'--proxy-server={proxy.proxy}')
+    driver = webdriver.Chrome(options=options)
+    proxy.new_har("500", options={'captureHeaders': True, 'captureContent': True})
+    driver.get("https://pizzeria.skillbox.cc/cart/")
+    for entry in proxy.har['log']['entries']:
+        request_url = entry['request']['url']
+        if 'pizzeria.skillbox.cc/cart' in request_url:
+            proxy.rewrite_url(request_url, "https://pizzeria.skillbox.cc/cart/500")
+            proxy.add_header(request_url, "HTTP/1.1 500 Internal Server Error")
+
+    yield driver
+    driver.quit()
+    server.stop()

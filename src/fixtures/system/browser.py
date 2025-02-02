05b4070ee1a5287a1512cd435bbbfe639676e84e
Sergey Romanov <dimetrius465@gmail.com>
2025-02-02 21:00:39 +0300

    changes

--- src/fixtures/system/browser.py
+++ src/fixtures/system/browser.py
@@ -61,11 +61,12 @@ def send_500_response():
     proxy = server.create_proxy()
     options = webdriver.ChromeOptions()
     options.add_argument(f'--proxy-server={proxy.proxy}')
-    driver = webdriver.Chrome(options=options)
+    service = Service(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     proxy.new_har("500", options={'captureHeaders': True, 'captureContent': True})
-    driver.get("https://pizzeria.skillbox.cc/cart/")
+    #driver.get("https://pizzeria.skillbox.cc/cart/")
     for entry in proxy.har['log']['entries']:
-        request_url = entry['request']['url']
+        request_url = entry['500']['pizzeria.skillbox.cc/cart']
         if 'pizzeria.skillbox.cc/cart' in request_url:
             proxy.rewrite_url(request_url, "https://pizzeria.skillbox.cc/cart/500")
             proxy.add_header(request_url, "HTTP/1.1 500 Internal Server Error")
